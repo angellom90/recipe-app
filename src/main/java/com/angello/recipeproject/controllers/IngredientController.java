97d9a51bfc84e61cc39627a799998d473ce1dbfc
Angello Manrique <angellom90@gmail.com>
2017-11-08 15:34:57 +0100

    adding list of ingredients

---
+++ src/main/java/com/angello/recipeproject/controllers/IngredientController.java
@@ -0,0 +1,35 @@
+package com.angello.recipeproject.controllers;
+
+import com.angello.recipeproject.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by jt on 6/28/17.
+ */
+@Slf4j
+@Controller
+public class IngredientController {
+
+    private final RecipeService recipeService;
+
+    // Construction Injection (DI)
+    public IngredientController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping
+    @RequestMapping("/recipe/{recipeId}/ingredients")
+    public String listIngredients(@PathVariable String recipeId, Model model){
+        log.debug("Getting ingredient list for recipe id: " + recipeId);
+
+        // use command object to avoid lazy load errors in Thymeleaf.
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
+
+        return "recipe/ingredient/list";
+    }
+}
