3a937832f37db66ea903c0c00343483937d1fad9
Angello Manrique <angellom90@gmail.com>
2017-11-10 12:41:50 +0100

    @ExceptionHandler implementation

--- src/main/java/com/angello/recipeproject/controllers/RecipeController.java
+++ src/main/java/com/angello/recipeproject/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package com.angello.recipeproject.controllers;
 
 import com.angello.recipeproject.commands.RecipeCommand;
+import com.angello.recipeproject.exceptions.NotFoundException;
 import com.angello.recipeproject.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by jt on 6/19/17.
@@ -56,5 +59,18 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(){
+
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
     }
 }
