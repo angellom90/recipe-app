5ca10bb7f9485e89cd65a45d6e43f38b8c071659
Angello Manrique <angellom90@gmail.com>
2017-11-13 11:11:47 +0100

    number format exception

--- src/test/java/com/angello/recipeproject/controllers/RecipeControllerTest.java
+++ src/test/java/com/angello/recipeproject/controllers/RecipeControllerTest.java
@@ -68,6 +68,17 @@ public class RecipeControllerTest {
     }
 
     @Test
+    public void testGetRecipeNumberFormat() throws Exception {
+
+        //not needed, don't reach
+        when(recipeService.findById(anyLong())).thenThrow(NumberFormatException.class);
+
+        mockMvc.perform(get("/recipe/asd/show"))
+                .andExpect(status(  ).isBadRequest())
+                .andExpect(view().name("404error"));
+    }
+
+    @Test
     public void testGetNewRecipeForm() throws Exception {
         RecipeCommand command = new RecipeCommand();
 
